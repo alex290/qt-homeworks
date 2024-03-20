05025a989558fd2755d4aa6f2283e8af3a54f3c0
alex <alex@webgoal.ru>
2024-03-20 18:20:50 +0300

    Update

--- 02/main.cpp
+++ 02/main.cpp
@@ -1,8 +1,22 @@
 #include <QCoreApplication>
+#include <QNetworkAccessManager>
+#include <QSqlDatabase>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+int main(int argc, char *argv[]) {
+  QCoreApplication a(argc, argv);
 
-    return a.exec();
+  // Ntework
+  QNetworkAccessManager *manager = new QNetworkAccessManager(NULL);
+
+  // DB
+  QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
+  db.setHostName("acidalia");
+  db.setDatabaseName("customdb");
+  db.setUserName("mojito");
+  db.setPassword("J0a1m8");
+  bool ok = db.open();
+
+  delete manager;
+
+  return a.exec();
 }
